a026a4ab50fb07b47af71afc17b549c667f60265
Satyendra Shukla <114739647+SatsNik@users.noreply.github.com>
2025-05-12 01:14:56 +0530

    Update settings.py

--- Sunlight/Sunlight/settings.py
+++ Sunlight/Sunlight/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [# Sunlight/Sunlight/settings.py
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'sunlight-11.onrender.com',  
+    '.onrender.com' 
+]]
 
 
 # Application definition
