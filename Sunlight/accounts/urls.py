f6aaeb5026960be60feb511904072bf69f451650
Satyendra Shukla <114739647+SatsNik@users.noreply.github.com>
2025-05-12 00:57:40 +0530

    Update urls.py

--- Sunlight/accounts/urls.py
+++ Sunlight/accounts/urls.py
@@ -3,6 +3,7 @@ from .views import register_view, login_view, logout_view, forgot_password_view,
 from django.contrib.auth import views as auth_views
 from . import views
 
+app_name = 'accounts'
 urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
